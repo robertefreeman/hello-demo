a3c4f4856c3e15465c659bf6c52f2253b723bdf7
Robert Freeman <6842762+robertefreeman@users.noreply.github.com>
2021-01-25 15:32:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def Welcome_name(name):
     return verbiage
   
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=False)
