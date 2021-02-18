9d788309a546f2c17222c9caf3838e5c9e0a2121
Robert Freeman <6842762+robertefreeman@users.noreply.github.com>
2021-02-18 11:23:07 -0500

    update app.py to reset demo
    reset demo ... both txt and reset security findings

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     hostname = socket.gethostname()
-    verbiage = 'Hello MSFT!!! You have been served by Countainer: ' + hostname
+    verbiage = 'Hello. You have been served by Countainer: ' + hostname
     return str(escape(verbiage))
                
 @app.route('/welcome/<name>')
 def Welcome_name(name):
     hostname = socket.gethostname()
     verbiage = 'Hello ' + name + '!! You have been server by Container: ' + hostname
-    return str(escape(verbiage))
+    return verbiage
   
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=False)
+    app.run(host="0.0.0.0", port=8000, debug=True)
